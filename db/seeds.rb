61b42a0ae7bd75a54afc7d359c03ba91c3c29347
Raffael Kniep <leus_raff@yahoo.de>
2018-05-15 15:14:37 +0200

    it works but looks shitty

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+tasks = Task.create([
+  {
+    title: "Laundry",
+    details: "Pick up the laundry, put it in the machine, wash it and hang it up to dry"
+  },
+  {
+    title: "Groceries",
+    details: "Check what you got, write a list, go shopping or dumpster-diving."
+  },
+  {
+    title: "Clean up",
+    details: "Take and broom and groom."
+  },
+  {
+    title: "Taxes",
+    details: "Earn money, then pay the state."
+  }
+])
+
